f94b6b1c6db50b9184fd1ecbc6b51b618c3a27c3
Justice Cupples <justicecupples5181@gmail.com>
2024-05-11 16:40:43 -0500

    validation

---
+++ scripts/validation.js
@@ -0,0 +1,41 @@
+function setEventListiners(formEl, options) {
+  const { inputSelector } = options;
+  const inputElm = [...formEl.querySelectorAll(inputSelector)];
+  inputElm.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+        console.dir(inputEl);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formElm = [...document.querySelectorAll(options.formSelector)];
+  formElm.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListiners(formEl, options);
+    // look for all inputs inside of form
+    // loop through all the inputs to see if all are valid
+    // if input is not valid
+    // get validation message
+    // add error class to input
+    // display error message
+    // disable button
+    // if all inputs are valid
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
